797deb6b81be7422f452e867ccea2afd7178f3f2
kyawlinnthant <kyawlinnthant@ilbcedu.com>
2024-05-02 09:52:09 +0630

    init[setup]: project init setup

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { auth } from "@/auth";
+
+import AuthButton from "./AuthButton.server";
+
+export default async function Home() {
+  const session = await auth();
+
+  return (
+    <main>
+      <h1 className="text-3xl font-bold">Home Page</h1>
+      <pre>{JSON.stringify(session, null, 2)}</pre>
+      <AuthButton />
+    </main>
+  );
+}
